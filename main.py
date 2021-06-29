e1fea0655b7f7cf242b0c15051663b68b7fa7435
Saiyd <koringajpgames@gmail.com>
2021-06-29 02:34:06 -0300

    melhorando a velocidade do loop

--- main.py
+++ main.py
@@ -5,13 +5,23 @@ import speech_recognition as sr
 
 # Criar reconhecedor 
 
-r = sr.Recognizer()
+
 
 # Abrir microfone para capturar audio
 
-with sr.Microphone() as source:
-    while True:
-        audio = r.listen(source) # Definir microfone como fonte
 
-        print('Ouvindo...')
-        print(r.recognize_google(audio, language='pt'))
+while True:
+    r = sr.Recognizer()
+    with sr.Microphone() as source: 
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source) # Definir microfone como fonte
+        
+        
+
+        try: 
+            print(r.recognize_google(audio, language='pt-BR', show_all=False))
+        except sr.UnknownValueError:
+            print("Audio não reconhecido")
+        except sr.RequestError as e:
+            print("Não foi possível obter resultados; {0}".format(e))
+        
